c41f6b19cdb758f133cc0497ca7fe5f2190b2fe3
andressaevelyn <contatoaevelyn@gmail.com>
2022-03-21 23:37:06 -0300

    declarei variaveis e calculei media

---
+++ declarando-variaveis.js
@@ -0,0 +1,9 @@
+const numero1=2;
+const numero2=7;
+
+const multiplicacaoDosNumeros= numero1*numero2;
+
+
+
+console.log (multiplicacaoDosNumeros);
+
---
+++ media-alunos.js
@@ -0,0 +1,8 @@
+const aluno1= 8;
+const aluno2=10;
+const aluno3= 5;
+
+const media= (aluno1+aluno2+aluno3)/3;
+console.log (media);
+
+
