2dc95859f7b1aaa94f1408c76801db0673bf0a3e
andressaevelyn <contatoaevelyn@gmail.com>
2022-03-28 22:08:42 -0300

    Utilizando função

--- qual-o-maior.js
+++ qual-o-maior.js
@@ -11,3 +11,16 @@ else if (numero1===numero2){
      console.log("O número "+ numero2+ " é maior");
     
  }
+ function qualOMaior(numero1, numero2) {
+    if (numero1>numero2) {
+        console.log (" O número " +numero1 +" é maior");
+    }
+    else if (numero1===numero2){
+        console.log("Os números são iguais");
+    }
+     else {
+         console.log("O número "+ numero2+ " é maior");
+     }
+    }
+
+    qualOMaior(10,15);
